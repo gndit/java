2d0583d23e3eef453f1128d3a16f106fb216ce01
ATUL KUMAR PATEL <45199617+gndit@users.noreply.github.com>
2019-08-25 22:55:02 +0530

    Add files via upload

---
+++ Atul.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Atul
+{
+	public static void main(String ar[])
+	{
+	int no;
+	Scanner ob1 = new Scanner(System.in);
+	System.out.println("enter any no");
+	
+	no = ob1.nextInt();
+	System.out.println("square of no is = " + no*no);	
+	System.out.println("bye");
+	}
+}
